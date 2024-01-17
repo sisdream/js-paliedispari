783ffa9107dd433c7c45ee49d1108c09596a3a49
sisdream <sissipascale2@gmail.com>
2024-01-17 15:41:28 +0100

    feat: funzione

--- numeri/js/main.js
+++ numeri/js/main.js
@@ -20,3 +20,30 @@ let numPc = genNumbRandom(1, 5);
 // sommo i numeri
 let somma = numUser + numPc;  
     
+//se la scelta dell'utente è uguale alla funzione numEven ha vinto
+  if(scelta === numEven(somma)){
+    console.log("hai vinto");
+    //altrimenti ha perso
+} else {
+    console.log("hai perso");
+}
+    
+// funzione generatrice di numeri random
+function genNumbRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+   
+// funzione per stabilire se un numero è pari
+function numEven(numF){
+   if(numF % 2 === 0){
+       return even = true;
+   } else {
+        return even = false;
+   }
+}  
+   
+//console log per un riscontro in pagina
+console.log("l'utente ha scelto: (se è true ha scelto pari se è false ha scelto dispari)", scelta);
+console.log("numero utente", numUser);
+console.log("numero cpu", numPc );
+console.log("la somma è:", somma);
