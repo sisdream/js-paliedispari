e3082fecaa563aa2cf6c048ef034e60d119f9774
sisdream <sissipascale2@gmail.com>
2024-01-17 15:12:58 +0100

    feat: valore

--- numeri/js/main.js
+++ numeri/js/main.js
@@ -4,3 +4,10 @@ const pariDispari = prompt("Scegli pari o dispari?");
 //creo una variabile per capire se l'utente ha scelto pari o dispari
 let scelta = "";
 
+//do un valore a "pari" o "dispari"
+    if(pariDispari === "pari"){
+        scelta = true;
+    }else(pariDispari === "dispari"){
+        scelta = false;
+    }
+    
